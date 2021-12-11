04da03b8a709b18ccb906fd0c8a4a4cde6fd15a0
iamomoro <iamomoroit@gmail.com>
2021-12-11 19:00:24 +0300

    filesystems upload

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,10 +43,10 @@ return [
         ],
 
         'admin' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'driver'     => 'local',
+            'root'       => public_path('uploads'),
             'visibility' => 'public',
+            'url' => env('APP_URL').'uploads/',
         ],
 
         's3' => [
